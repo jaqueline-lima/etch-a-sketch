5f4a4b04408e1aad8475d3c5dcaa922951ad9703
Jaqueline <jaquelinedlp78@gmail.com>
2022-03-28 21:18:03 -0300

    Fixed it so that the pixels remain painted.

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ function createGrid() {
         square.classList.add('square');
         square.style.cssText = `width: ${squareSide}px;`, `height: ${squareSide}px;`;
         container.appendChild(square);
+ /*       square.addEventListener('mouseover', () => {
+            square.classList.add('hover');
+        }) */
     }
+/*
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(addEventListener('mouseover', () => {
+        squares.classList.add('hover');
+    })) */
 }
 
 createGrid();
