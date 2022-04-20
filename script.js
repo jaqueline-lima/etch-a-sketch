7499c3e065a4fdbfe4481103c3d0b1c4a8b1c4de
Jaqueline <jaquelinedlp78@gmail.com>
2022-04-20 16:37:35 -0300

    Added rainbow option.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function createGrid(size) {
         square.classList.add('square');
         square.style.cssText = `width: ${squareSide}px;`, `height: ${squareSide}px;`;
         container.appendChild(square);
-    }
+    } 
 }
 
 function clearGrid() {
@@ -30,20 +30,47 @@ function clearGrid() {
     createGrid(size);
     const squares = document.querySelectorAll('.square').forEach(square => {
         square.addEventListener('mouseover', () => {
-            square.classList.add('color');
+            colorSquare(square, squaresColor);
         })
     });
 }
 
+function makeRainbow(square) {
+    let r,
+        g,
+        b;
+
+    r = Math.random() * 256 >> 0;
+    g = Math.random() * 256 >> 0;
+    b = Math.random() * 256 >> 0;
+
+    square.style.cssText += `background-color: rgb(${r}, ${g}, ${b});`
+}
+
+function colorSquare(square, squaresColor) {
+    if (squaresColor === 'black') {
+        square.style.cssText += 'background-color: black';
+    } else if (squaresColor === 'rb') {
+        makeRainbow(square);
+    }
+}
+
 createGrid(16);
+
+let squaresColor = 'black';
+
+const rb = document.querySelector('#rb');
+rb.addEventListener('click', () => {
+    squaresColor = 'rb';
+});
 
 const squares = document.querySelectorAll('.square').forEach(square => {
     square.addEventListener('mouseover', () => {
-        square.classList.add('color');
+        colorSquare(square, squaresColor);
     })
 });
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => {
     clearGrid();
-})
+});
