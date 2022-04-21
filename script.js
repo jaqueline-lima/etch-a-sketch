b0871ae969edc3625a67eee91275a252f9b60960
Jaqueline <jaquelinedlp78@gmail.com>
2022-04-21 12:32:21 -0300

    Added button to make the squares black again.

--- script.js
+++ script.js
@@ -7,6 +7,42 @@ function getSize() {
     return size;
 }
 
+function clearGrid() {
+    const container = document.querySelector('.container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    size = getSize();
+    createGrid(size);
+
+    const squares = document.querySelectorAll('.square').forEach(square => {
+        square.addEventListener('mouseover', () => {
+            colorSquare(square, squaresColor);
+        }) 
+    });
+}
+
+function makeRainbow() {
+    let r,
+        g,
+        b;
+
+    r = Math.random() * 256 >> 0;
+    g = Math.random() * 256 >> 0;
+    b = Math.random() * 256 >> 0;
+
+    return `background-color: rgb(${r}, ${g}, ${b});`
+}
+
+function colorSquare(square, squaresColor) {
+    if (squaresColor === 'black') {
+        square.style.cssText += 'background-color: black';
+    } else if (squaresColor === 'rb') {
+        let rb = makeRainbow();
+        square.style.cssText += rb;
+    }
+}
+
 function createGrid(size) {
     const container = document.querySelector('.container');
     let square,
@@ -19,49 +55,26 @@ function createGrid(size) {
         square.style.cssText = `width: ${squareSide}px;`, `height: ${squareSide}px;`;
         container.appendChild(square);
     } 
-    const squares = document.querySelectorAll('.square').forEach(square => {
-        square.addEventListener('mouseover', () => {
-            colorSquare(square, squaresColor);
-        }) 
-    });
-}
-
-function clearGrid() {
-    const container = document.querySelector('.container');
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    size = getSize();
-    createGrid(size);
-}
-
-function makeRainbow(square) {
-    let r,
-        g,
-        b;
-
-    r = Math.random() * 256 >> 0;
-    g = Math.random() * 256 >> 0;
-    b = Math.random() * 256 >> 0;
-
-    square.style.cssText += `background-color: rgb(${r}, ${g}, ${b});`
-}
-
-function colorSquare(square, squaresColor) {
-    if (squaresColor === 'black') {
-        square.style.cssText += 'background-color: black';
-    } else if (squaresColor === 'rb') {
-        makeRainbow(square);
-    }
-}
+} 
 
 createGrid(16);
 
 let squaresColor = 'black';
 
+const squares = document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('mouseover', () => {
+        colorSquare(square, squaresColor);
+    }) 
+});
+
 const rb = document.querySelector('#rb');
 rb.addEventListener('click', () => {
     squaresColor = 'rb';
+});
+
+const blackButton = document.querySelector('#black');
+blackButton.addEventListener('click', () => {
+    squaresColor = 'black';
 });
 
 const clearButton = document.querySelector('#clear');
